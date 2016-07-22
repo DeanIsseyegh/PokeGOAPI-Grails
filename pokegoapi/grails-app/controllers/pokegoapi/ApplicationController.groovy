e6f52c86fddcda128f2d5eb911e6eef1eba929ec
Dean Isseyegh <dean.isseyegh@secretescapes.com>
2016-07-22 12:26:29 +0100

    Got first working prototype

--- pokegoapi/grails-app/controllers/pokegoapi/ApplicationController.groovy
+++ pokegoapi/grails-app/controllers/pokegoapi/ApplicationController.groovy
@@ -1,8 +1,12 @@
 package pokegoapi
 
+import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass
+import com.pokegoapi.api.PokemonGo
+import com.pokegoapi.api.map.Map
+import com.pokegoapi.auth.GoogleLogin
 import grails.core.GrailsApplication
-import grails.util.Environment
 import grails.plugins.*
+import okhttp3.OkHttpClient
 
 class ApplicationController implements PluginManagerAware {
 
@@ -10,6 +14,24 @@ class ApplicationController implements PluginManagerAware {
     GrailsPluginManager pluginManager
 
     def index() {
+		println "test3"
+		OkHttpClient httpClient = new OkHttpClient()
+		String token = ""
+		RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo auth = new GoogleLogin(httpClient).login(token)
+		PokemonGo go = new PokemonGo(auth,httpClient)
+
+		println "Player info:"
+		println go.getPlayerProfile()
+
+		go.latitude = 51.517959999999995 as Double
+		go.longitude = -0.10827279999999999 as Double
+
+		Map map = new Map(go)
+		def mapObjects = map.getMapObjects()
+
+		println "Map Objects:\n ${mapObjects}"
+
+
         [grailsApplication: grailsApplication, pluginManager: pluginManager]
     }
 }
