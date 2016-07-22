06e8b71cbbf77cf02ae0ce162c8f23f7e705dec5
OsaSoft <OsaSofto@gmail.com>
2016-07-22 14:28:36 +0100

    getting token from ext config and constructing go object on startup

--- pokegoapi/grails-app/controllers/pokegoapi/ApplicationController.groovy
+++ pokegoapi/grails-app/controllers/pokegoapi/ApplicationController.groovy
@@ -7,31 +7,27 @@ import com.pokegoapi.auth.GoogleLogin
 import grails.core.GrailsApplication
 import grails.plugins.*
 import okhttp3.OkHttpClient
+import org.springframework.beans.factory.annotation.*
+
+import javax.annotation.PostConstruct
 
 class ApplicationController implements PluginManagerAware {
 
-    GrailsApplication grailsApplication
+	def grailsApplication
     GrailsPluginManager pluginManager
 
-    def index() {
-		println "test3"
+	static allowedMethods = [areaInfo: "GET"]
+
+	@Value('${pokemon.go.player.token}')
+	def token
+	def go
+
+	@PostConstruct
+	def init() {
 		OkHttpClient httpClient = new OkHttpClient()
-		String token = ""
 		RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo auth = new GoogleLogin(httpClient).login(token)
-		PokemonGo go = new PokemonGo(auth,httpClient)
+		go = new PokemonGo(auth, httpClient)
+	}
 
-		println "Player info:"
-		println go.getPlayerProfile()
-
-		go.latitude = 51.517959999999995 as Double
-		go.longitude = -0.10827279999999999 as Double
-
-		Map map = new Map(go)
-		def mapObjects = map.getMapObjects()
-
-		println "Map Objects:\n ${mapObjects}"
-
-
-        [grailsApplication: grailsApplication, pluginManager: pluginManager]
-    }
+    def index() {}
 }
