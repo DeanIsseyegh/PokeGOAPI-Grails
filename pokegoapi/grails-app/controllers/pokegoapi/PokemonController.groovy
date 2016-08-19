500553075471a1de00486b6b227a94974b0e9f78
Dean Isseyegh <dean.isseyegh@secretescapes.com>
2016-08-19 10:32:53 +0100

    Added endpoint for updating the database

--- pokegoapi/grails-app/controllers/pokegoapi/PokemonController.groovy
+++ pokegoapi/grails-app/controllers/pokegoapi/PokemonController.groovy
@@ -6,6 +6,12 @@ class PokemonController {
 
 	static allowedMethods = [nearbyPokemon: "GET"]
 
+	def updateData() {
+		println "Starting at ${new Date().time}"
+		pokeradarService.updateData()
+		println "Finished at ${new Date().time}"
+	}
+
 	def nearbyPokemon() {
 		def nearbyPokemon = (params.lat && params.lon) ? pokemonService.getPokemonForLocation(params.lat, params.lon) : []
 		[nearbyPokemons: nearbyPokemon.collect{it.pokemonId}]
