686cf59950165fee5e1a59be10dd62d5751320eb
OsaSoft <OsaSofto@gmail.com>
2016-09-02 17:33:17 +0100

    Heroku deploy setup and trying to get the fucking this working, with no luck... Problem with getting the external config in Application.groovy setEnvironment

--- pokegoapi/grails-app/init/pokegoapi/Application.groovy
+++ pokegoapi/grails-app/init/pokegoapi/Application.groovy
@@ -2,12 +2,9 @@ package pokegoapi
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
 import org.springframework.context.EnvironmentAware
 import org.springframework.core.env.Environment
-import org.springframework.core.env.PropertiesPropertySource
-import org.springframework.core.io.FileSystemResource
-import org.springframework.core.io.Resource
+import org.springframework.core.env.MapPropertySource
 
 class Application extends GrailsAutoConfiguration implements EnvironmentAware {
     static void main(String[] args) {
@@ -16,14 +13,18 @@ class Application extends GrailsAutoConfiguration implements EnvironmentAware {
 
     @Override
     void setEnvironment(Environment environment) {
-        String configPath = System.properties["local.config.location"]
-        Resource resourceConfig = new FileSystemResource(configPath)
-        if (resourceConfig.exists()) {
-            YamlPropertiesFactoryBean ypfb = new YamlPropertiesFactoryBean()
-            ypfb.setResources([resourceConfig] as Resource[])
-            ypfb.afterPropertiesSet()
-            Properties properties = ypfb.getObject()
-            environment.propertySources.addFirst(new PropertiesPropertySource("local.config.location", properties))
+        //Set up Configuration directory
+        def confFolder = "conf/"
+
+        println ""
+        println "Loading configuration from ${confFolder}"
+        def appConfigured = new File('pokegoapi-config.groovy').exists()
+        println "Loading configuration file ${new File('pokegoapi-config.groovy')}"
+        println "Config file found : " + appConfigured
+
+        if (appConfigured) {
+            def config = new ConfigSlurper().parse(new File('pokegoapi-config.groovy').toURL())
+            environment.propertySources.addFirst(new MapPropertySource("pokegoapi-config", config))
         }
     }
 }
